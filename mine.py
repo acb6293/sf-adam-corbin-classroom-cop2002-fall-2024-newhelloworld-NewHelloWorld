8ca534f0c6e8f1e587e8761505706f1d6135cbcd
Alexandria Biedron <alexandria.biedron1@gmail.com>
2024-08-30 17:24:48 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,7 +1,9 @@
 # Write a line of code that prints "Hello world!".
-
+print ("Hello world!")
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  
+  welcome = "Hello World!"
+
+print(welcome)
   return variable_name
